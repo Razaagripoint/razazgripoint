65a096a503d0e577d3135613a180137bcd491330
Mushahid Raza <itsmushahid07@gmail.com>
2026-06-26 22:20:44 +0530

    Upgrade cache version and modify cached URLs
    Updated cache version and added manifest.json to cache.

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = "rap-cache-v1";
+const CACHE_NAME = "rap-cache-v2";
+
 const urlsToCache = [
   "/razaagripoint/",
-  "/razaagripoint/index.html"
+  "/razaagripoint/index.html",
+  "/razaagripoint/manifest.json"
 ];
 
+// Install
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
@@ -12,6 +17,24 @@ self.addEventListener("install", event => {
   );
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
